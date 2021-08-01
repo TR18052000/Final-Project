56758f44676ebe414c9f5f41aa35edced6b0b412
TR18052000 <88049818+TR18052000@users.noreply.github.com>
2021-08-01 15:17:21 +0530

    Add files via upload
    Project Submitted

---
+++ Temp Conversion.py
@@ -0,0 +1,12 @@
+# Temperature Converter from Celsius to Fahrenheit
+
+Celsius=float(input("Enter temperature in celsius:")) #  The user emterd the desired temperature in celsius
+
+Fahrenheit=(Celsius * 1.8)+32  # (°C × 9/5) + 32 = °F  °C-->Temperature in celsius  °F-->Temperature in Fahrenheit
+
+# The above formula is applied to convert the degree Celsius to degree fahrenheit                                                      
+
+print("Temperature in fahrenheit:",Fahrenheit) # The Temperature converted from degree Celsius to degree Fahrenheit is displayed
+
+# End Of the Code
+
